ef646f19131f5b21d59eb1624f7f86a81d6e44c9
bryantomoregie <bryantomoregie@yahoo.com>
2020-07-02 20:22:04 -0500

    DNA incomplete

---
+++ complementarydna.js
@@ -0,0 +1,14 @@
+function DNAStrand(dna){
+    //turn string to array 
+    //use if, else if 
+    //if 
+  }
+
+
+//   function DNAStrand(dna){
+//     var arr = dna.split('')
+//     arr.map(x => if(x == a) {
+//         x = t
+//     } )
+//   }
+
--- descendingorder.js
+++ descendingorder.js
@@ -13,3 +13,8 @@ function descendingOrder(n){
   var n = 21445
 
   console.log(descendingOrder(n))
+
+  //did it in one line for the first time !
+  //.sort() will sort an array in alphabetical order or ascending order
+ // if you want descending you need to do .sort((a, b) => b - a)
+ 
