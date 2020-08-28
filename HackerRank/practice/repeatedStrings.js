33af8eebfac4e03a010dceb4927751f751c1f409
bryantomoregie <unclebryant@gmail.com>
2020-08-28 15:59:18 -0500

    saved

---
+++ HackerRank/practice/repeatedStrings.js
@@ -0,0 +1,29 @@
+
+
+function repeatedString(s, n) {
+let arr = s.split('');
+let string = ''
+
+for(let i = 0; i <= n; i++){
+        string = string.concat()
+    }
+return string
+
+    
+}
+
+
+
+
+
+console.log(repeatedString('aba',
+    10))
+
+
+    /*
+    .split
+    for of loop 
+    concat until string.length is === n
+
+    for loop and count the a's of new string
+    */
