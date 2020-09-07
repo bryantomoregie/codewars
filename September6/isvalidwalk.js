02dd908c8b409a991c882edbaf6e7fb207bd7dc4
bryantomoregie <unclebryant@gmail.com>
2020-09-07 00:09:10 -0500

    validwalk complete

--- September6/isvalidwalk.js
+++ September6/isvalidwalk.js
@@ -1,40 +1,22 @@
 //https://www.codewars.com/kata/54da539698b8a2ad76000228/train/javascript
 
 function isValidWalk(walk) {
-    // if (walk.length  !== 10) return false
+    if (walk.length  !== 10) return false
     let obj = {};
-    let value = null
 
     for(let i = 0; i <= walk.length - 1; i++){
         obj[walk[i]] = (obj[walk[i]] || 0) + 1;
     }
 
-    // for(let val in obj){
-    //     if (obj[val] !== 5){
-    //         return false
-    //     } else {
-    //         continue
-    //     }
-    // }
+    if ((Object.keys(obj).length === 2) && (obj.n === obj.s)) return true;
+    if ((Object.keys(obj).length === 4) && (obj.e === obj.w)) return true; 
 
-   
-
-    if (obj.n == undefined) return true  
-
-    // for(let val in obj){
-    //     if (obj[val] !== value ){
-    //         return false 
-    //     } else {
-    //         return true
-    //     }
-    // }
-  
-
+    return false 
   
   }
 
 
-console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s'])) //true;
-console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e'])) //false;
+console.log(isValidWalk(['n','s','n','s','e','w','e','w','e','e'])) //true;
+
 
 //check to see if all the values are equal 
